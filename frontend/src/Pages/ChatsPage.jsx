4a697e8ea1116be974b6bfed156440168c867970
Ankush Ladani <astarladani@gmail.com>
2022-08-31 17:23:15 +0530

    Started Chat Page's UI

--- frontend/src/Pages/ChatsPage.jsx
+++ frontend/src/Pages/ChatsPage.jsx
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-const ChatsPage = () => {
-  const [chats, setChats] = useState([]);
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chat");
-    setChats(data);
-  };
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/layout";
+import MyChats from "../Components/MyChats.js";
+import ChatBox from "../Components/ChatBox.js";
+import SideDrawer from "../Components/misc/SideDrawer";
 
-  useEffect(() => {
-    fetchChats();
-  }, []);
+const ChatsPage = () => {
+  const { user } = ChatState();
 
   return (
     <div>
-      {chats.map((chat) => (
-        <div key={chat._id}> {chat.chatName} </div>
-      ))}
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
     </div>
   );
 };
